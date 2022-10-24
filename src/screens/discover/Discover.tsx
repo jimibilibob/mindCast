b7700522fbb3a1a1704df613d5aefc1ff1cba911
Israel Aguilar <isra.neri2@gmail.com>
2022-10-24 12:30:22 -0400

    Add react-native-video and set the iOS background tasks. Add Android jcenter repo, settings.gradle react-native-video dependency and project android/app/build.gradle appcompat dependency

--- src/screens/discover/Discover.tsx
+++ src/screens/discover/Discover.tsx
@@ -7,8 +7,10 @@ import CardAuthor from './CardAuthor';
 import HeaderSection from './HeaderSection';
 import CardHottest from './CardHottest';
 import PlayerFragment from 'components/PlayerFragment/PlayerFragment';
+import Video from 'react-native-video';
 
 const Discover = () => {
+    let player
   return (
     <ScrollView style={styles.container}>
         <Text
@@ -32,6 +34,16 @@ const Discover = () => {
             data={Array.from({ length: 20 })}
             renderItem={(item) => <CardHottest/>}/>
         <PlayerFragment/>
+        <Video source={{uri: 'https://stream-33.zeno.fm/avr8uenbm1duv?zs=rWoYZThiTGK9-SbkK6nUzA'}}   // Can be a URL or a local file.
+            ref={(ref) => {
+                player = ref
+            }}                                      // Store reference
+            // onBuffer={this.onBuffer}                // Callback when remote video is buffering
+            // onError={this.videoError}               // Callback when video cannot be loaded
+            audioOnly
+            ignoreSilentSwitch={'ignore'}
+            playInBackground // To play in background
+            style={styles.backgroundVideo} />
     </ScrollView>
   )
 }
@@ -47,5 +59,12 @@ const styles = StyleSheet.create({
         color: '#FFF',
         marginLeft: 10,
         marginTop: 10
-    }
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      }
 })
